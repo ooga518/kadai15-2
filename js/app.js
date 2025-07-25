45218ab723e2d55b76875289e57baaa62b534642
ooga518 <2401136@s.asojuku.ac.jp>
2025-07-25 10:11:10 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,28 +1,28 @@
-//読み込み完了後に実行する
+// 読み込み完了後に実行する
 $(function() {
     console.log('jQueryの準備ができました。ページ読み込み完了。');
+
     // ボタンがクリックされたときの処理
     $('#ajaxButton').on('click', function() {
-        
+
         $.ajax({
-
-            //リクエスト先のURLを設定（作成１)
-            url: ,
-
-        }).done(function(data){
-
-            //JSONデータを解析
+            // リクエスト先のURLを設定（作成１）
+            url: 'api.php',
+            type: 'POST', // POSTでもGETでも良いが統一してPOSTに
+            data: {} // 今回は送信するデータなし
+        }).done(function(data) {
+            // JSONデータを解析
             let result = JSON.parse(data);
-            //解析データを整形
-            // result.messageとresult.timeを改行で結合（作成２)
-            
 
+            // result.message と result.time を改行で結合（作成２）
+            let text = result.message + '\n' + result.time;
+
+            // 結果を表示
             $("#ajax_return").text(text);
 
-        }).fail(function(data){
-            //通信が失敗したときの処理
+        }).fail(function() {
             alert("通信に失敗しました");
         });
- 
+
     });
 });
